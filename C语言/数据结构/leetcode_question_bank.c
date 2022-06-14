210b30997c59e65b2fdaf4ebce70054d8753f0b8
yuanquan <yuanquan@star-net.cn>
2022-06-14 11:20:14 +0800

    [ADD] 添加LeetCode题库3、35题的实现

--- C语言/数据结构/leetcode_question_bank.c
+++ C语言/数据结构/leetcode_question_bank.c
@@ -23,7 +23,7 @@ struct ListNode{
  * 你可以按任意顺序返回答案。
  *
  * 难度：简单
- * 解法：暴力解法。推荐使用链表的方式
+ * 思路：暴力解法。推荐使用链表的方式
  **/
 int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
     *returnSize = 2;
@@ -48,7 +48,7 @@ OUT:
  * 你可以假设除了数字 0 之外，这两个数都不会以 0 开头。
  *
  * 难度：中等
- * 解法：采用链表的方式
+ * 思路：采用链表的方式
  **/
 struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2) {
     struct ListNode* head = NULL;
@@ -101,7 +101,7 @@ struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2) {
  * 给你一个整数 x ，如果 x 是一个回文整数，返回 true ；否则，返回 false 。回文数是指正序（从左向右）和倒序（从右向左）读都是一样的整数。
  * 
  * 难度：简单
- * 解法：
+ * 思路：
  **/
 bool isPalindrome(int x) {
     bool is = TRUE;
@@ -130,7 +130,7 @@ bool isPalindrome(int x) {
  * 编写一个函数来查找字符串数组中的最长公共前缀。如果不存在公共前缀，返回空字符串 ""。
  * 
  * 难度：简单
- * 解法：
+ * 思路：
  **/
 char * longestCommonPrefix(char ** strs, int strsSize) {
     if (!strsSize) {
@@ -165,7 +165,7 @@ char * longestCommonPrefix(char ** strs, int strsSize) {
  * 将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。 
  * 
  * 难度：简单
- * 解法：递归法
+ * 思路：递归法
  **/
 struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2){
     if (list1 == NULL) {
@@ -189,7 +189,7 @@ struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2){
  * 给定一个已排序的链表的头 head ， 删除所有重复的元素，使每个元素只出现一次 。返回 已排序的链表 。
  * 
  * 难度：简单
- * 解法：
+ * 思路：
  **/
 struct ListNode* deleteDuplicates(struct ListNode* head){
     if (head == NULL || head->next == NULL) {
@@ -217,7 +217,7 @@ struct ListNode* deleteDuplicates(struct ListNode* head){
  * 给定一个非空整数数组，除了某个元素只出现一次以外，其余每个元素均出现两次。找出那个只出现了一次的元素。
  * 
  * 难度：简单
- * 解法：题中明确提出每个元素均出现两次，按位异或(^=)两次后保持不变
+ * 思路：题中明确提出每个元素均出现两次，按位异或(^=)两次后保持不变
  **/
 int singleNumber(int* nums, int numsSize){
     int reslut = nums[0];
@@ -227,4 +227,53 @@ int singleNumber(int* nums, int numsSize){
     }
 
     return reslut;
+}
+
+/* 35. 搜索插入位置
+ * 给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+ * 请必须使用时间复杂度为 O(log n) 的算法。
+ * 
+ * 难度：简单
+ * 思路：
+ **/
+int searchInsert(int* nums, int numsSize, int target){
+    for (int i = 0; i < numsSize; i++) {
+        if (nums[i] >= target) {
+            return i;
+        }
+    }
+
+    return numsSize;
+}
+
+/* 3. 无重复字符的最长子串
+ * 给定一个字符串 s ，请你找出其中不含有重复字符的 最长子串 的长度。
+ *
+ * 难度：中等
+ * 思路：采用滑动窗口的方式解决
+ **/
+int lengthOfLongestSubstring(char * s){
+    if (s == NULL) {
+        return 0;
+    }
+
+    int slen  = strlen(s);
+    int left  = 0;
+    int right = -1;
+    int max   = right - left +1;
+
+    for (int i = 0; i < slen; i++) {
+        for (int j = left; j <= right; j++) {
+            if (s[i] == s[j]) {
+                left = j + 1;
+                break;
+            }
+        }
+        right++;
+        if (max <= right - left +1) {
+            max = right - left + 1;
+        }
+    }
+
+    return max;
 }
